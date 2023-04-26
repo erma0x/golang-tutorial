1cfe3729a0b0e51f74390c2bc832c6cb3470dc59
erma0x <35337869+erma0x@users.noreply.github.com>
2023-04-26 23:46:58 +0200

    Update functions.go

--- functions.go
+++ functions.go
@@ -6,9 +6,15 @@ import "fmt"
 // define a function
 func greet() {
   fmt.Println("Good Morning")
-}
 
+// function to add two numbers
+func addNumbers(n1 int, n2 int) {
+  sum := n1 + n2
+  fmt.Println("Sum:", sum)
+}
+ 
 
 func main() {
-
+  // function call
+  addNumbers()
 }
